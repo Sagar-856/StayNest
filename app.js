279bb90e01ba17aceb7b276e53f5388f8b4d735b
sagar <sagartambole856@gmail.com>
2026-04-14 11:22:42 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,95 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const Listing = require("../MAJORPROJECT/models/listing.js");
+const path = require("path");
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/staynest";
+
+app.set("view-engine","ejs");
+app.set("views",path.join(__dirname,"views"));
+app.use(express.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
+app.engine('ejs', ejsMate);
+app.use(express.static(path.join(__dirname,"/public")));
+
+
+main().then( () => {
+    console.log("Connected to DB");
+}).catch( err => { console.log(err)});
+
+async function main() {
+    await mongoose.connect(MONGO_URL);
+}
+
+//INDEX ROUTE
+app.get("/listings", async (req, res) => {
+    let allListings = await  Listing.find({ });
+    res.render("listings/index.ejs",{ allListings });
+});
+
+//NEW ROUTE
+app.get("/listings/new",(req, res) => {
+    res.render("listings/new.ejs");
+});
+
+//SHOW ROUTE
+app.get("/listings/:id", async(req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/show.ejs",{ listing });
+});
+
+//CREATE ROUTE
+app.post("/listings", async (req, res) => {
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+})
+
+//EDIT ROUTE
+app.get("/listings/:id/edit", async(req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit.ejs",{ listing });
+})
+
+//Update Route
+app.put("/listings/:id", async(req, res) => {
+    let { id } = req.params;
+    await Listing.findByIdAndUpdate(id, {...req.body.listing});
+    res.redirect(`/listings/${id}`);
+})
+
+//Delete Route
+app.delete("/listings/:id", async(req, res) => {
+    let { id } =req.params;
+    let listing = await Listing.findByIdAndDelete(id);
+    console.log(listing);
+    res.redirect("/listings");
+})
+
+
+// app.get("/testListing", async(req, res) => {
+//     let sampleListing = new Listing({
+//         title: "My New Villa",
+//         description: "by the beach",
+//         price: 1500,
+//         location: "calangutt, Goa",
+//         country: "India"
+//     });
+//     await sampleListing.save();
+//     console.log("sample was saved");
+//     res.send("succesfully saved");
+// })
+
+
+app.get("/",(req,res) => {
+    res.send("im working");
+})
+
+app.listen(8700,() => {
+    console.log("server is running on port 8700");
+})
---
+++ models/listing.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: String,
+    image: {
+        filename: {
+            type: String,
+            default: "listingimage"
+        },
+        url: {
+            type: String,
+            default: "https://i0.wp.com/picjumbo.com/wp-content/uploads/beautiful-nature-mountain-scenery-with-flowers-free-photo.jpg?w=600&quality=80"
+        }
+    },
+    // image: {
+    //     type: String,
+    //     default:"https://i0.wp.com/picjumbo.com/wp-content/uploads/beautiful-nature-mountain-scenery-with-flowers-free-photo.jpg?w=600&quality=80",
+    //     set: (v) => v === "" ? "https://i0.wp.com/picjumbo.com/wp-content/uploads/beautiful-nature-mountain-scenery-with-flowers-free-photo.jpg?w=600&quality=80" : v,
+    // } ,
+    price: Number,
+    location: String,
+    country: String
+})
+
+const Listing = mongoose.model("Listing",listingSchema);
+module.exports = Listing;
